f4d0ce4f2e175e57d9f968b43b891981d9f351a8
rcmtcristian <rcmtcristian@gmail.com>
2023-04-25 02:58:36 -0400

    feat: first commit

---
+++ server/routes/index.js
@@ -0,0 +1,44 @@
+// This file references all of your different routers into one router to be used by our controllers
+
+// Require Dependencies
+const path = require('path')
+const router = require('express').Router()
+
+// Require your router directory here ================
+
+// You need to require all of the routers that are being exported from each file here
+// The below example is for all of your API routes. Since we are using React,
+// there is little need for an '/html' file so for the most part, you can just leave this file be
+// and move on to the api directory.
+
+// Example:
+const apiRoutes = require('./api')
+
+// ===================================================
+
+// Tell the express router to use your routes here====
+
+// Example:
+router.use('/api', apiRoutes)
+// Your API has now picked up '/api' as a reference to the file
+// For more information about how this api is set up, please reference README.md
+
+// ===================================================
+
+// When an unknown route is hit, send the React app
+// If application is in production,
+if (process.env.NODE_ENV === 'production') {
+  // Send build index.html
+  router.use(function (req, res) {
+    res.sendFile(path.join(__dirname, '../../client/build/index.html'))
+  })
+} else {
+  // else if the application is in development,
+  // Send the public index.html
+  router.use(function (req, res) {
+    res.sendFile(path.join(__dirname, '../../client/public/index.html'))
+  })
+}
+
+// Exports Router
+module.exports = router
---
+++ vite.config.ts
@@ -0,0 +1,49 @@
+/** @type {import('vite').UserConfig} */
+
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { VitePluginFonts } from 'vite-plugin-fonts'
+import { createHtmlPlugin } from 'vite-plugin-html'
+import viteImagemin from 'vite-plugin-imagemin'
+
+import config from './config.js'
+import postcss from './postcss.config.js'
+
+const { imagemin } = config
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    viteImagemin(imagemin),
+    createHtmlPlugin({
+      minify: true,
+      entry: '/client/src/main.tsx'
+    }),
+    VitePluginFonts({
+      // Custom fonts
+      custom: {
+        families: [
+          {
+            name: 'CascadiaCodePL',
+            src: './client/src/assets/fonts/*.woff2'
+          }
+        ],
+        display: 'swap',
+        preload: true,
+        prefetch: false,
+        injectTo: 'head-prepend'
+      }
+    })
+  ],
+  css: {
+    postcss
+  },
+  resolve: {
+    alias: [
+      { find: '@/', replacement: '/client/src' },
+      { find: '@/Assets', replacement: '/client/src/assets' },
+      { find: '@/Components', replacement: '/client/src/components' }
+    ]
+  }
+})
