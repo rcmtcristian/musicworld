33e9cac94cf8b577215203458e5b24840e87fb3b
Cris <rcmtcristian@gmail.com>
2023-11-17 08:53:59 -0500

    Revert "💄 Style: set up x-small @media"

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,31 +1,33 @@
 function Footer() {
   return (
-    <footer className="footer-container">
-      <div className="footer-container-top">
-        <p>
-          <span className="">
-            © 2023 <a href="/">rcmtcristian</a> Music world aims to provide easy access to finding
-            similar music to your favorite songs. We have a large database of songs and artist that
-            you can search through to find similar music to your favorite songs. we also have a
-            database of artist that you can search through to find similar music to your favorite
-            artist.
-          </span>
-        </p>
-      </div>
-      <div className="footer-container-bottom">
-        <div className="col-md-6">
-          <br />
-          <ul className="list-inline">
-            <li>
-              <a href="#">Website</a>
-            </li>
-            <li>
-              <a href="#">Twitter</a>
-            </li>
-            <li>
-              <a href="#">Github</a>
-            </li>
-          </ul>
+    <footer>
+      <div className="footer-container">
+        <div className="footer-container-top">
+          <p>
+            <span className="">
+              © 2023 <a href="/">rcmtcristian</a> Music world aims to provide easy access to finding
+              similar music to your favorite songs. We have a large database of songs and artist
+              that you can search through to find similar music to your favorite songs. we also have
+              a database of artist that you can search through to find similar music to your
+              favorite artist.
+            </span>
+          </p>
+        </div>
+        <div className="footer-container-bottom">
+          <div className="col-md-6">
+           <br />
+            <ul className="list-inline">
+              <li>
+                <a href="#">Website</a>
+              </li>
+              <li>
+                <a href="#">Twitter</a>
+              </li>
+              <li>
+                <a href="#">Github</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </footer>
